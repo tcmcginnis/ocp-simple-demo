fbbced604da2ffa59a9afa56e18718b37c31ffb0
root <root@bastion.mclabs.us>
2023-01-20 14:14:26 -0500

    New demo

---
+++ install-ocp.sh
@@ -0,0 +1,7 @@
+kubectl create -f apple.yml
+kubectl create -f banana.yml
+kubectl create -f pear.yml
+kubectl create -f route-apple.yml
+kubectl create -f route-banana.yml
+kubectl create -f route-pear.yml
+# kubectl create -f replica-pear.yml
---
+++ install.sh
@@ -0,0 +1,8 @@
+kubectl create -f apple.yml
+kubectl create -f banana.yml
+kubectl create -f pear.yml
+kubectl create -f ingress-apple.yml
+kubectl create -f ingress-banana.yml
+kubectl create -f ingress-pear.yml
+kubectl create -f ingress_path.yml
+# kubectl create -f replica-pear.yml
