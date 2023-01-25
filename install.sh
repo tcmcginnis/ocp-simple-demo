111047d3450904ed970c28fc1745f9eb10021479
root <root@bastion.mclabs.us>
2023-01-25 18:53:41 -0500

    slim

--- install.sh
+++
@@ -1,8 +0,0 @@
-kubectl create -f apple.yml
-kubectl create -f banana.yml
-kubectl create -f pear.yml
-kubectl create -f ingress-apple.yml
-kubectl create -f ingress-banana.yml
-kubectl create -f ingress-pear.yml
-kubectl create -f ingress_path.yml
-# kubectl create -f replica-pear.yml
